3f124d1229c85c8e69919df8151779982f9461e9
John Cowie <john.a.cowie@gmail.com>
2012-10-12 10:37:43 +0100

    Cleaned repo for restart of Ruby guide

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Ruby-Spike
 ==========
 
 Spiking Ruby
+
+Created a blog application by following the steps at http://guides.rubyonrails.org/getting_started.html#creating-the-blog-application (up to step 6 so far)
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = 'e0c0a148c8c66e06d66371b19e2d8bf4543ec8fbe05683c47b0816fc0981c9a8380f072cfe99038dddb4503c24f089a585e0629899e34d3562a16def5e3319c4'
