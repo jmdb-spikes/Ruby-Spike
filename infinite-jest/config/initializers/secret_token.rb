d5025d10c21abbb2db89b0f5028483d55468bf07
John Cowie <john.a.cowie@gmail.com>
2012-10-12 12:45:50 +0100

    Duplicated code and made client and server. Client makes HTTP requests, server makes DB requests

--- infinite-jest/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-InfiniteJest::Application.config.secret_token = '9512501bce641365874bf8dfee6ce3126f0d2c6311d7f1f8c4669c2533d127699a49828457522f46dae9e21937afd8791a478ad71d4018043619d0202b356e19'
