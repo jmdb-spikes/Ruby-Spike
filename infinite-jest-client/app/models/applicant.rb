b8e2fb634eb1804a9273bf33e15838b34f3d104e
John Cowie <john.a.cowie@gmail.com>
2012-10-15 11:02:14 +0100

    Trying to make Applicant extend ActiveModel so it can be used in a similar way to ActiveResource

--- infinite-jest-client/app/models/applicant.rb
+++ infinite-jest-client/app/models/applicant.rb
@@ -1,4 +1,36 @@
 class Applicant 
-  extend ActiveModel::Naming
-  attr_accessor :id, :firstname, :lastname, :age, :claimDate, :model_name
+  include ActiveModel::Validations
+  include ActiveModel::Serialization
+  include ActiveSupport::JSON
+  
+  attr_accessor :id, :firstname, :lastname, :age, :claimDate
+  
+  attr_accessor :attributes
+  def initialize(attributes = {})
+    @attributes = attributes
+  end
+  
+  def read_attribute_for_validation(key)
+    @attributes[key]
+  end
+  
+  def self.all
+     json = RestClient.get 'http://localhost:3001/applicants.json'
+     resources = JSON.parse(json)
+    @applicants = []
+    resources.each { |resource| 
+        @applicants.push(Applicant.from_hash(resource)) 
+    } 
+    return @applicants
+  end
+  
+  def self.from_hash(hash)
+    applicant = Applicant.new
+    applicant.firstname = hash['firstname']
+    applicant.lastname = hash['lastname']
+    applicant.age = hash['age']
+    applicant.claimDate = hash['claimDate']
+    applicant.id = hash['id']
+    return applicant  
+  end
 end
