46b38d3a8c80a0f5e14457c2fa65032d08379386
Ilias Bartolini <iliasbartolini@gmail.com>
2012-10-15 17:49:05 +0100

    Ilias: minor cleanup and renaming

--- infinite-jest-client/app/models/applicant.rb
+++ infinite-jest-client/app/models/applicant.rb
@@ -1,22 +1,22 @@
-class Applicant 
+class Applicant
   extend ActiveModel::Naming
   include ActiveModel::Validations
   include ActiveModel::Conversion
   include ActiveModel::Serialization
   include ActiveSupport::JSON
-  
+
   attr_accessor :id, :firstname, :lastname, :age, :claimDate, :created_at, :updated_at
-  
+
   def initialize(attributes = {})
-    attributes.each do |name, value|  
+    attributes.each do |name, value|
       send("#{name}=", value)
     end
   end
-  
+
   def read_attribute_for_validation(key)
     @attributes[key]
   end
-  
+
   def persisted?
     self.id != nil
   end
@@ -25,17 +25,17 @@ class Applicant
   #  "#{id}="
   #end
 
-  def self.find(id) 
+  def self.find(id)
     json = RestClient.get 'http://localhost:3001/applicants/'+id+'.json', :content_type => :json, :accept => :json
     resource = JSON.parse(json)
     return Applicant.new(resource)
   end
-  
+
   def self.all
-     json = RestClient.get 'http://localhost:3001/applicants.json', :content_type => :json, :accept => :json
-     resources = JSON.parse(json)
+    json = RestClient.get 'http://localhost:3001/applicants.json', :content_type => :json, :accept => :json
+    resources = JSON.parse(json)
     @applicants = []
-    resources.each { |resource| 
+    resources.each { |resource|
       @applicants.push(Applicant.new(resource))
     }
     return @applicants
@@ -45,41 +45,41 @@ class Applicant
     p self
     RestClient.post('http://localhost:3001/applicants.json', self.to_json, :content_type => :json, :accept => :json) { |response, request, result, &block|
       case response.code
-      when 201
-        true
-      when 422
-        parse_errors(response)
-        false
-      else
-        response.return!(request, result, &block)
+        when 201
+          true
+        when 422
+          _parse_errors(response)
+          false
+        else
+          response.return!(request, result, &block)
       end
     }
   end
 
   def update_attributes(params)
-    merge_attributes(params)
+    _merge_attributes(params)
 
     RestClient.put('http://localhost:3001/applicants/'+self.id.to_s+'.json', self.to_json, :content_type => :json, :accept => :json) { |response, request, result, &block|
       case response.code
-      when 200
-        true
-      when 422
-        parse_errors(response)
-        false
-      else
-        response.return!(request, result, &block)
+        when 200
+          true
+        when 422
+          _parse_errors(response)
+          false
+        else
+          response.return!(request, result, &block)
       end
     }
   end
 
-  def parse_errors(response)
+  def _parse_errors(response)
     errors = JSON.parse(response.to_str)
     errors.each { |key, value|
       self.errors.add(key, value)
     }
   end
 
-  def merge_attributes(attributes)
+  def _merge_attributes(attributes)
     attributes.each do |name, value|
       send("#{name}=", value)
     end
